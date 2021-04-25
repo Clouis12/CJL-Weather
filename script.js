184737796f2be07a4b083ac14d1cc471845ed27a
Christian Louis <77083360+Clouis12@users.noreply.github.com>
2021-04-24 23:51:17 -0500

    Updated API Keys

--- script.js
+++ script.js
@@ -79,7 +79,7 @@ window.addEventListener('load', function () {
   // Helper function to fetch and display the UV index
   function getUVIndex(lat, lon) {
     fetch(
-      `http://api.openweathermap.org/data/2.5/uvi?appid=d91f911bcf2c0f925fb6535547a5ddc9&lat=${lat}&lon=${lon}`
+      `http://api.openweathermap.org/data/2.5/uvi?appid=15038671c45c9502c274d6b86b0d82bb&lat=${lat}&lon=${lon}`
     )
       .then((res) => res.json())
       .then((data) => {
@@ -121,7 +121,7 @@ window.addEventListener('load', function () {
 
   // Function that preforms the actual API request and creates elements to render to the page
   function searchWeather(searchValue) {
-    var endpoint = `http://api.openweathermap.org/data/2.5/weather?q=${searchValue}&appid=d91f911bcf2c0f925fb6535547a5ddc9&units=imperial`;
+    var endpoint = `http://api.openweathermap.org/data/2.5/weather?q=${searchValue}&appid=15038671c45c9502c274d6b86b0d82bb&units=imperial`;
     fetch(endpoint)
       .then((res) => res.json())
       .then((data) => {
